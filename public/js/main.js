5d0807b683765f5ba98138c269de8f4b995309e1
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-17 15:38:23 +0100

    Exo 2

--- public/js/main.js
+++ public/js/main.js
@@ -6,12 +6,21 @@ let listItems = document.getElementsByTagName('li');
 function sayMyName(li){
     alert(li.innerText);
 }
-for(let i = 0; i < listItems.length; i++){
-    listItems[i].addEventListener('click', () =>{
-        sayMyName(listItems[i]);
-    })
-}
+// for(let i = 0; i < listItems.length; i++){
+//     listItems[i].addEventListener('click', () =>{
+//         sayMyName(listItems[i]);
+//     })
+// }
 
 // # 2. En utilisant le meme procédé qu'au dessus Créer un programme qui permet de modifier le contenu de chaque Li en double cliquant dessus via un prompt(attention peut-être que tu devras commenter l'exercice précédent)
+function modifieMyName(li){
+    let nouveauNom = prompt("Entrez le contenu du li");
+    return nouveauNom;
+}
+for(let i = 0; i < listItems.length; i++){
+    listItems[i].addEventListener('dblclick', () =>{
+        listItems[i].innerText = modifieMyName(listItems[i]);
+    })
+}
 
 // # 3. Créer un programme qui permet d'inverser l'ordre de deux li ! avec l'aide des inputs et du button inverser
