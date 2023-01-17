0498b2ab365aedab106b9c898a91389a6d030c11
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-17 15:29:25 +0100

    Exo 1

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,16 @@
 
 // # 1. En utilisant une boucle pour appliquer un écouteur d'événement sur chaque "li", créer un programme qui permet de lancer une function du nom de sayMyName qui affiche une alert du texte du li sur le quel on clique.
 
+let listItems = document.getElementsByTagName('li');
+function sayMyName(li){
+    alert(li.innerText);
+}
+for(let i = 0; i < listItems.length; i++){
+    listItems[i].addEventListener('click', () =>{
+        sayMyName(listItems[i]);
+    })
+}
+
 // # 2. En utilisant le meme procédé qu'au dessus Créer un programme qui permet de modifier le contenu de chaque Li en double cliquant dessus via un prompt(attention peut-être que tu devras commenter l'exercice précédent)
 
 // # 3. Créer un programme qui permet d'inverser l'ordre de deux li ! avec l'aide des inputs et du button inverser
